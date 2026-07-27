022db6a9cc9ca21a0bf809000ce1d060adda65e1
DonData <lange@outlook.de>
2026-07-27 10:58:48 +0200

    Rename schema to silver, add stg_matches, clean up dbt_project.yml

---
+++ transform/models/staging/stg_matches.sql
@@ -0,0 +1,25 @@
+select
+    id as match_id,
+    utc_date as match_utc_date,
+    status,
+    matchday,
+    stage,
+
+    home_team__id as home_team_id,
+    home_team__name as home_team_name,
+    home_team__short_name as home_team_short_name,
+
+    away_team__id as away_team_id,
+    away_team__name as away_team_name,
+    away_team__short_name as away_team_short_name,
+
+    competition__id as competition_id,
+    competition__name as competition_name,
+
+    score__winner as score_winner,
+    score__full_time__home as full_time_home_goals,
+    score__full_time__away as full_time_away_goals,
+    score__half_time__home as half_time_home_goals,
+    score__half_time__away as half_time_away_goals
+
+from {{ source('bronze', 'matches') }}
